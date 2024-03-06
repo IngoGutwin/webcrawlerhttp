dcb853f8ec6e405f5a0308e185bed928b5fd7546
Ingo Gutwin <onmabu@gmail.com>
2024-03-06 21:05:46 +0100

    crawl singlepage with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,29 @@
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(currentURL) {
+    console.log(`activly crawling: ${currentURL}`);
+
+    try {
+        const resp = await fetch(currentURL);
+
+        if (await resp.status > 399) {
+            console.log(`error in fetch: ${resp.status}, on page: ${currentURL}`);
+            return;
+        };
+
+        const contentType = resp.headers.get('content-type');
+        if (!contentType.includes("text/html")) {
+            console.log(`not html response, content type: ${contentType}, on page: ${currentURL}`);
+            return;
+            
+        }
+
+        console.log(await resp.text());
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`);
+    }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = [];
     const dom = new JSDOM(htmlBody);
@@ -36,6 +60,7 @@ function normalizeURL(urlstring) {
 }
 
 module.exports = {
+    crawlPage,
     getURLsFromHTML,
     normalizeURL,
 }
